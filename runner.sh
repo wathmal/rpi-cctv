c62f9133eec3c5811493fb5a9c79729edc38de5d
Sasitha Sonnadara <Sasitha.Sonnadara@trycake.com>
2019-02-11 19:47:29 +0530

    updater runner script.

--- runner.sh
+++ runner.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 
 # install deps
-#sudo apt-get install jq curl
+sudo apt-get install jq curl
 
 # check if process is running on 4000
 # if so attempt to kill
 
 # run onvif detector server
-#nohup node onvif.js &
+nohup node onvif.js &
 
-#curl -X GET \
-#  http://localhost:4000 \
-#  -H 'cache-control: no-cache' \
-#  -o cameras.txt
 
 list=$(curl -X GET http://localhost:4000)
-#list='["rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov", "rtsp://localhost:8554/demo1"]'
 
 for row in $(echo "${list}" | jq '.[]'); do
     _jq() {
